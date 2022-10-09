7f3bb7f97ce1b77bfd976f0572afcd643458a8ca
Ilya Sukhov <ilya.sukhov@rewe-digital.com>
2022-10-09 17:59:41 +0200

    NOTICKET Add starter project

---
+++ src/main/kotlin/com/rewedigital/timeslotexercise/TimeslotExerciseApplication.kt
@@ -0,0 +1,11 @@
+package com.rewedigital.timeslotexercise
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TimeslotExerciseApplication
+
+fun main(args: Array<String>) {
+	runApplication<TimeslotExerciseApplication>(*args)
+}
---
+++ src/test/kotlin/com/rewedigital/timeslotexercise/TimeslotExerciseApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.rewedigital.timeslotexercise
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TimeslotExerciseApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
