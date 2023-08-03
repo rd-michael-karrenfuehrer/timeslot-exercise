75f394240319a33a30cadda2ee818dbbf2cff48b
Michael.Karrenführer <michael.karrenfuehrer@rewe-digital.com>
2023-08-03 09:23:30 +0200

    NOTICKET add H2, JPA and lombok support

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
 	id("io.spring.dependency-management") version "1.1.1"
 	kotlin("jvm") version "1.9.0"
 	kotlin("plugin.spring") version "1.9.0"
+	kotlin("plugin.jpa") version "1.9.0"
 }
 
 group = "com.rewe-digital"
@@ -19,8 +20,10 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("org.projectlombok:lombok:1.18.28")
 	testImplementation("org.springframework.boot:spring-boot-starter-test") {
 		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
 		exclude(group = "junit", module = "junit")
@@ -28,6 +31,7 @@ dependencies {
 	testImplementation("org.apache.groovy:groovy-all:4.0.13")
 	testImplementation("org.spockframework:spock-core:2.4-M1-groovy-4.0")
 	testImplementation("org.spockframework:spock-spring:2.4-M1-groovy-4.0")
+	runtimeOnly("com.h2database:h2:2.2.220")
 }
 
 tasks.withType<KotlinCompile> {
